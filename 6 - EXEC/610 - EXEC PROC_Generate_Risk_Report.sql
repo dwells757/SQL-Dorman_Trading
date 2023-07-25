66a05d1cb9f8dcf6825dc836b2ced01dd7104cfe
dwells757 <84671936+dwells757@users.noreply.github.com>
2023-07-25 06:52:59 -0500

    Commit 20230725

--- 6 - EXEC/610 - EXEC PROC_Generate_Risk_Report.sql
+++ 6 - EXEC/610 - EXEC PROC_Generate_Risk_Report.sql
@@ -4,6 +4,6 @@ GO
 SET NOCOUNT ON
 
 EXEC [dbo].[PROC_Generate_Risk_Report]
-								@Processing_Date='7/19/23' --@Processing_Date DATE=NULL
+								--@Processing_Date='7/19/23' --@Processing_Date DATE=NULL
 
 SET NOCOUNT OFF
