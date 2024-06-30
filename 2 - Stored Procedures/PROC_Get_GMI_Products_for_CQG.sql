ae10c900d8e07efa535ba94284589d480a2ce02b
dwells757 <david.hardesty@abnamroclearing.com>
2024-06-30 10:18:53 -0500

    PROC_Get_GMI_Products_for_CQG 2

--- 2 - Stored Procedures/PROC_Get_GMI_Products_for_CQG.sql
+++ 2 - Stored Procedures/PROC_Get_GMI_Products_for_CQG.sql
@@ -11,6 +11,7 @@ SET QUOTED_IDENTIFIER ON
 GO
 
 CREATE PROCEDURE [dbo].[PROC_Get_GMI_Products_for_CQG]
+												@IN_GMI_to_CQG_Mappings_YN VARCHAR(1)='Y'
 
 AS
 
@@ -25,14 +26,19 @@ SET NOCOUNT ON
 -- Create #GMIPOSF1_File_Current
 --***********************************************************************************************************
 CREATE TABLE #GMIPOSF1_File_Current
-(PEXCH VARCHAR(2),
-PFC VARCHAR(2),
-PCTYM VARCHAR(6),
+(GMI_Exchange VARCHAR(2),
+GMI_Symbol VARCHAR(2),
+GMI_Type VARCHAR(1),
+Expiry VARCHAR(8),
+PSDSC1 VARCHAR(255),
 PCLOSE NUMERIC(15,8),
-GMI_Description VARCHAR(30),
+GMIPOS_Records INT,
+IN_GMI_to_CQG_Mappings_YN VARCHAR(1) DEFAULT 'N',
+GMI_LONGDESC VARCHAR(255) DEFAULT '',
 CQG_Symbol VARCHAR(10),
 CQG_Exchange VARCHAR(100),
 CQG_Description VARCHAR(100),
+CQG_Contract VARCHAR(100),
 YY VARCHAR(2) DEFAULT '??',
 MM VARCHAR(2) DEFAULT '??',
 Month_Code VARCHAR(1) DEFAULT '?',
@@ -41,26 +47,60 @@ CQG_Product VARCHAR(20) DEFAULT 'UNDEFINED')
 --***********************************************************************************************************
 -- Load #GMIPOSF1_File_Current
 --***********************************************************************************************************
-INSERT INTO #GMIPOSF1_File_Current(PEXCH,PFC,PCTYM,PCLOSE,GMI_Description,CQG_Symbol,CQG_Exchange,CQG_Description)VALUES('06','27','202405',99.49000000,'ICEUS SUGAR11','SBE','ICE Futures US (NBE)','Sugar World #11 -- NYBOT/ICE')
-INSERT INTO #GMIPOSF1_File_Current(PEXCH,PFC,PCTYM,PCLOSE,GMI_Description,CQG_Symbol,CQG_Exchange,CQG_Description)VALUES('06','43','202405',94.66000000,'ICEUS COFFEEC','KCE','ICE Futures US (NBE)','Coffee -- NYBOT/ICE')
-INSERT INTO #GMIPOSF1_File_Current(PEXCH,PFC,PCTYM,PCLOSE,GMI_Description,CQG_Symbol,CQG_Exchange,CQG_Description)VALUES('06','43','202412',87.92000000,'ICEUS COFFEEC','KCE','ICE Futures US (NBE)','Coffee -- NYBOT/ICE')
-INSERT INTO #GMIPOSF1_File_Current(PEXCH,PFC,PCTYM,PCLOSE,GMI_Description,CQG_Symbol,CQG_Exchange,CQG_Description)VALUES('06','43','202503',78.28000000,'ICEUS COFFEEC','KCE','ICE Futures US (NBE)','Coffee -- NYBOT/ICE')
-INSERT INTO #GMIPOSF1_File_Current(PEXCH,PFC,PCTYM,PCLOSE,GMI_Description,CQG_Symbol,CQG_Exchange,CQG_Description)VALUES('06','CY','202405',90.81000000,'ICEUS COFFEEC','CCE','ICE Futures US (NBE)','Cocoa -- NYBOT/ICE')
-INSERT INTO #GMIPOSF1_File_Current(PEXCH,PFC,PCTYM,PCLOSE,GMI_Description,CQG_Symbol,CQG_Exchange,CQG_Description)VALUES('06','CY','202407',104.56200000,'ICEUS COFFEEC','CCE','ICE Futures US (NBE)','Cocoa -- NYBOT/ICE')
-INSERT INTO #GMIPOSF1_File_Current(PEXCH,PFC,PCTYM,PCLOSE,GMI_Description,CQG_Symbol,CQG_Exchange,CQG_Description)VALUES('06','CY','202409',197.75000000,'ICEUS COFFEEC','CCE','ICE Futures US (NBE)','Cocoa -- NYBOT/ICE')
-INSERT INTO #GMIPOSF1_File_Current(PEXCH,PFC,PCTYM,PCLOSE,GMI_Description,CQG_Symbol,CQG_Exchange,CQG_Description)VALUES('06','CY','202412',195.60000000,'ICEUS COFFEEC','CCE','ICE Futures US (NBE)','Cocoa -- NYBOT/ICE')
-INSERT INTO #GMIPOSF1_File_Current(PEXCH,PFC,PCTYM,PCLOSE,GMI_Description,CQG_Symbol,CQG_Exchange,CQG_Description)VALUES('06','RS','202411',195.65000000,'ICEUS CANOLA','WR','ICE Futures US-Canadian Grains (WCE)','Canola (Rape Seed) -- ICECAN')
-INSERT INTO #GMIPOSF1_File_Current(PEXCH,PFC,PCTYM,PCLOSE,GMI_Description,CQG_Symbol,CQG_Exchange,CQG_Description)VALUES('13','28','202405',368.75000000,'ICEUS COTTON2','CTE','ICE Futures US (NBE)','Cotton -- NYBOT/ICE')
-INSERT INTO #GMIPOSF1_File_Current(PEXCH,PFC,PCTYM,PCLOSE,GMI_Description,CQG_Symbol,CQG_Exchange,CQG_Description)VALUES('13','96','202405',22.32000000,'ICEUS FCOJ-A','OJE','ICE Futures US (NBE)','Orange Juice A(FCOJ-A) -- NYBOT/ICE')
-INSERT INTO #GMIPOSF1_File_Current(PEXCH,PFC,PCTYM,PCLOSE,GMI_Description,CQG_Symbol,CQG_Exchange,CQG_Description)VALUES('13','XD','202406',652.10000000,'ICEUS USD INDX','DXE','ICE Futures US (NBE)','Dollar Index -- NYBOT/ICE')
+INSERT INTO #GMIPOSF1_File_Current
+(GMI_Exchange,
+GMI_Symbol,
+GMI_Type,
+Expiry,
+PSDSC1,
+PCLOSE,
+GMIPOS_Records)
+SELECT
+PEXCH, --GMI_Exchange,
+PFC, --GMI_Symbol,
+PTYPE, --GMI_Type,
+RTRIM(PCTYM) + RTRIM(PSBCUS), --Expiry,
+PSDSC1,
+PCLOSE,
+COUNT(*) --GMIPOS_Records
+FROM [dbo].[GMIPOSF1_File_Raw]
+WHERE (PSTYPE='F')
+AND (PTYPE='')
+--AND (PSBCUS<>'')
+GROUP BY
+PEXCH,
+PFC,
+PTYPE,
+PCTYM,
+PSBCUS,
+PSDSC1,
+PCLOSE
+
+--***********************************************************************************************************
+-- Update #GMIPOSF1_File_Current
+-- with [dbo].[GMI_to_CQG_Mappings]
+--***********************************************************************************************************
+UPDATE A
+SET
+IN_GMI_to_CQG_Mappings_YN='Y',
+GMI_LONGDESC=B.GMI_LONGDESC,
+CQG_Symbol=B.CQG_Symbol,
+CQG_Exchange=B.CQG_Exchange,
+CQG_Description=B.CQG_Description,
+CQG_Contract=B.CQG_Contract
+FROM #GMIPOSF1_File_Current A
+INNER JOIN [dbo].[GMI_to_CQG_Mappings] B
+ON (A.GMI_Exchange=B.GMI_Exchange)
+AND (A.GMI_Symbol=B.GMI_Symbol)
+AND (A.GMI_Type=B.GMI_Type)
 
 --***********************************************************************************************************
 -- Generate YY and MM
 --***********************************************************************************************************
 UPDATE #GMIPOSF1_File_Current
 SET
-YY = SUBSTRING(PCTYM,3,2),
-MM = SUBSTRING(PCTYM,5,2)
+YY = SUBSTRING(Expiry,3,2),
+MM = SUBSTRING(Expiry,5,2)
 
 --***********************************************************************************************************
 -- Generate Month_Code
@@ -89,18 +129,17 @@ SET Month_Code =
 UPDATE #GMIPOSF1_File_Current
 SET CQG_Product = 'F.US.' + REPLACE(CQG_Symbol,' ','') + Month_Code + YY
 
---SELECT *
---FROM #GMIPOSF1_File_Current
-
 --***********************************************************************************************************
 -- Create #GMI_Products_for_CQG
 --***********************************************************************************************************
 CREATE TABLE #GMI_Products_for_CQG
 (Record_Number INT IDENTITY(1,1),
-PEXCH VARCHAR(2),
-PFC VARCHAR(2),
-PCTYM VARCHAR(6),
-GMI_Description VARCHAR(30),
+GMI_Exchange VARCHAR(2),
+GMI_Symbol VARCHAR(2),
+GMI_Type VARCHAR(1),
+Expiry VARCHAR(8),
+PSDSC1 VARCHAR(255),
+GMI_LONGDESC VARCHAR(255),
 CQG_Symbol VARCHAR(10),
 CQG_Exchange VARCHAR(100),
 CQG_Description VARCHAR(100),
@@ -112,41 +151,39 @@ CQG_Price NUMERIC(18,9) DEFAULT 0)
 -- Load #GMI_Products_for_CQG
 --***********************************************************************************************************
 INSERT INTO #GMI_Products_for_CQG
-(PEXCH,
-PFC,
-PCTYM,
-GMI_Description,
+(GMI_Exchange,
+GMI_Symbol,
+GMI_Type,
+Expiry,
+PSDSC1,
+GMI_LONGDESC,
 CQG_Symbol,
 CQG_Exchange,
 CQG_Description,
 CQG_Product,
 PCLOSE)
 SELECT
-PEXCH,
-PFC,
-PCTYM,
-GMI_Description,
+GMI_Exchange,
+GMI_Symbol,
+GMI_Type,
+Expiry,
+PSDSC1,
+GMI_LONGDESC,
 CQG_Symbol,
 CQG_Exchange,
 CQG_Description,
 CQG_Product,
 PCLOSE
 FROM #GMIPOSF1_File_Current
+WHERE (IN_GMI_to_CQG_Mappings_YN=@IN_GMI_to_CQG_Mappings_YN)
 ORDER BY
-PEXCH,
-PFC,
-PCTYM
-
---SELECT
-----'F.US.' + REPLACE(CQG_Symbol,' ','') "CQG_Symbol"
---'F.US.' + REPLACE(CQG_Symbol,' ','') + 'K24' "CQG_Symbol"
---FROM [dbo].[GMI_Contracts]
---WHERE (CQG_Symbol='SBE') --DWH Test 4/2/24
---ORDER BY CQG_Symbol
+GMI_Exchange,
+GMI_Symbol,
+GMI_Type,
+Expiry
 
 DROP TABLE #GMIPOSF1_File_Current
 
---SELECT TOP 4 *
 SELECT *
 FROM #GMI_Products_for_CQG
 ORDER BY Record_Number
--- 7 - Scripts/5 - Load GMI_Contracts.sql
+++ 7 - Scripts/5 - Load GMI_Contracts.sql
@@ -14,12 +14,16 @@ PRINT '*************************************************************************
 --INSERT INTO [dbo].[GMI_Contracts](PEXCH,PFC,PTYPE,GMI_Description,CQG_Symbol,CQG_Exchange,CQG_Description)VALUES('06','43','','ICEUS COFFEEC','KCE','ICE Futures US (NBE)','Coffee -- NYBOT/ICE')
 INSERT INTO [dbo].[GMI_Contracts](PEXCH,PFC,GMI_Description,CQG_Symbol,CQG_Exchange,CQG_Description)VALUES('06','27','ICEUS SUGAR11','SBE','ICE Futures US (NBE)','Sugar World #11 -- NYBOT/ICE')
 INSERT INTO [dbo].[GMI_Contracts](PEXCH,PFC,GMI_Description,CQG_Symbol,CQG_Exchange,CQG_Description)VALUES('06','43','ICEUS COFFEEC','KCE','ICE Futures US (NBE)','Coffee -- NYBOT/ICE')
-INSERT INTO [dbo].[GMI_Contracts](PEXCH,PFC,GMI_Description,CQG_Symbol,CQG_Exchange,CQG_Description)VALUES('06','CY','ICEUS COFFEEC','CCE','ICE Futures US (NBE)','Cocoa -- NYBOT/ICE')
+INSERT INTO [dbo].[GMI_Contracts](PEXCH,PFC,GMI_Description,CQG_Symbol,CQG_Exchange,CQG_Description)VALUES('06','CY','ICEUS COCOA','CCE','ICE Futures US (NBE)','Cocoa -- NYBOT/ICE')
 INSERT INTO [dbo].[GMI_Contracts](PEXCH,PFC,GMI_Description,CQG_Symbol,CQG_Exchange,CQG_Description)VALUES('06','RS','ICEUS CANOLA','WR','ICE Futures US-Canadian Grains (WCE)','Canola (Rape Seed) -- ICECAN')
 
 INSERT INTO [dbo].[GMI_Contracts](PEXCH,PFC,GMI_Description,CQG_Symbol,CQG_Exchange,CQG_Description)VALUES('13','28','ICEUS COTTON2','CTE','ICE Futures US (NBE)','Cotton -- NYBOT/ICE')
 INSERT INTO [dbo].[GMI_Contracts](PEXCH,PFC,GMI_Description,CQG_Symbol,CQG_Exchange,CQG_Description)VALUES('13','96','ICEUS FCOJ-A','OJE','ICE Futures US (NBE)','Orange Juice A(FCOJ-A) -- NYBOT/ICE')
 INSERT INTO [dbo].[GMI_Contracts](PEXCH,PFC,GMI_Description,CQG_Symbol,CQG_Exchange,CQG_Description)VALUES('13','XD','ICEUS USD INDX','DXE','ICE Futures US (NBE)','Dollar Index -- NYBOT/ICE')
+
+INSERT INTO [dbo].[GMI_Contracts](PEXCH,PFC,GMI_Description,CQG_Symbol,CQG_Exchange,CQG_Description)VALUES('16','AD','IMM AUS $','DA6','CME (GLOBEX) (GLO)','Australian Dollar -- GLOBEX')
+INSERT INTO [dbo].[GMI_Contracts](PEXCH,PFC,GMI_Description,CQG_Symbol,CQG_Exchange,CQG_Description)VALUES('16','BP','IMM B-POUND','BP6','CME (GLOBEX) (GLO)','British Pound -- GLOBEX')
+INSERT INTO [dbo].[GMI_Contracts](PEXCH,PFC,GMI_Description,CQG_Symbol,CQG_Exchange,CQG_Description)VALUES('16','ES','EMINI S&P 500','EP','CME (GLOBEX) (GLO)','S&P 500 Index - E-Mini -- GLOBEX')
 
 SELECT COUNT(*) "Records Loaded"
 FROM [dbo].[GMI_Contracts]
